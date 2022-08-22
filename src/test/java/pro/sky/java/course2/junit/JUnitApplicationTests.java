99cb868ea0d703b2b59065d82fb5b20c9bf73d19
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-08-22 13:26:42 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/junit/JUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
